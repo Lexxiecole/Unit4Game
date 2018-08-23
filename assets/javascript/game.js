24cbaf1aa94e77dbb455119af83100a84401e384
Alexandra <Lexxiecole@gmail.com>
2018-08-23 16:48:02 -0400

    entered some psuedo coding

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+
+
+
+//PSUEDOCODING
+
+//first photo
+
+//random number is assigned to the random number box
+
+//crystal is clicked
+
+//random number is assigned to crystal---i probably should limit the numbers that can be chosen ( a certain height)
+
+//random number assigned to crystal is shown in box???
+
+//each time a crystal is clicked, its random number adds to the total score
+
+//if the total score reaches the same exact number as the random number chosen, a win happens
+
+//if the total score reaches a higher number than the random number, a loss happens.
+
+
+
+//make sure all photos work together to add numbers??
